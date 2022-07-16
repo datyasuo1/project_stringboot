402dbc5a7b12739760201648917fcbd54fa17abc
Đạt.Code <73332200+datyasuo1@users.noreply.github.com>
2022-07-16 15:31:24 +0700

    .

--- exam/src/main/java/com/example/exam/api/ProductApi.java
+++ exam/src/main/java/com/example/exam/api/ProductApi.java
@@ -1,4 +1,32 @@
 package com.example.exam.api;
 
+
+import com.example.exam.entity.Product;
+import com.example.exam.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/products")
 public class ProductApi {
+    @Autowired
+    ProductService productService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Iterable<Product>> getList(){
+        return ResponseEntity.ok(productService.findAll());
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Product product){
+        return ResponseEntity.ok(productService.save(product));
+    }
+
+
 }
--- exam/src/main/java/com/example/exam/api/SaleApi.java
+++ exam/src/main/java/com/example/exam/api/SaleApi.java
@@ -1,4 +1,30 @@
 package com.example.exam.api;
 
+import com.example.exam.entity.Product;
+import com.example.exam.entity.Sale;
+import com.example.exam.service.ProductService;
+import com.example.exam.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/sales")
 public class SaleApi {
+    @Autowired
+    SaleService saleService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Iterable<Sale>> getList(){
+        return ResponseEntity.ok(saleService.findAll());
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Sale sale){
+        return ResponseEntity.ok(saleService.save(sale));
+    }
+
 }
